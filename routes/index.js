0ecc353ded091129ab2cf5a981fa5bf7688aa166
Gabriel Csapo <gacsapo@paypalcorp.com>
2015-11-27 15:30:51 -1000

    adds cache back

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ module.exports = function(app) {
 
 	app.get('/', function(req, res) {
 		res.render('index', function(err, html) {
-			// global.cache[req.originalUrl] = {
-			// 	response: html,
-			// 	type: 'html'
-			// };
+			global.cache[req.originalUrl] = {
+				response: html,
+				type: 'html'
+			};
 			res.send(html);
 		});
 	});
