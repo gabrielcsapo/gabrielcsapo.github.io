323e1837c37a327e3de3d2405c14a2a3e0d6a131
Gabriel Csapo <gacsapo@paypalcorp.com>
2015-12-01 13:53:26 -0800

    can now handle posts in db.json

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,39 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
-		res.render('index', function(err, html) {
-			// global.cache[req.originalUrl] = {
-			// 	response: html,
-			// 	type: 'html'
-			// };
+		var posts = global.db('posts').value();
+		posts.forEach(function(post) {
+			if(post.type == 'img' && post.file !== undefined && post.src == undefined) {
+				post.src = fs.readFileSync(path.join(__dirname, '../public', post.file)).toString('base64');
+			}
+		});
+		res.render('index', {
+			posts: posts 
+		}, function(err, html) {
+			global.cache[req.originalUrl] = {
+				response: html,
+				type: 'html'
+			};
+			res.send(html);
+		});
+	});
+
+	app.get('/:link', function(req, res) {
+		var link = req.params.link;
+		var post = global.db('posts')
+					  .chain()
+					  .find({link: link})
+					  .value();
+		res.render('post', {
+			post: post 
+		}, function(err, html) {
+			global.cache[req.originalUrl] = {
+				response: html,
+				type: 'html'
+			};
 			res.send(html);
 		});
 	});
